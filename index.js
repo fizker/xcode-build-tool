779a576a2919c453fa4ed60f0035ea375421a60d
Benjamin Horsleben <benjamin@fizkerinc.dk>
2012-09-26 08:47:46 +0200

    Renamed the main file. Added a shell-script with a slightly better name. Made the config file a command-line parameter instead of hard-wired

---
+++ index.js
@@ -0,0 +1,97 @@
+#!/usr/bin/env node
+
+var exec = require('child_process').execFile
+  , path = require('path')
+  , fs = require('fs')
+
+  , provisions = require('./src/provisions')
+
+  , baseDir = path.join(__dirname, '..')
+
+  , conf
+
+if(!process.argv[2]) {
+	var executable = process.env.BUILD_EXEC || path.basename(process.argv[1])
+	console.log('Use as: %s path/to/config.json', executable)
+	process.exit(1)
+}
+conf = require(process.argv[2])
+
+process.env.BASEDIR = baseDir
+process.env.KEYCHAIN_LOCATION = conf.keychain.path
+process.env.KEYCHAIN_PASSWORD = conf.keychain.password
+
+var jobs =
+    [ parseProvisions
+    , installProvisions
+    , addKeychain
+    , unlockKeychain
+    , buildTarget
+    , clean
+    ]
+  , nextJob = 0
+
+executeNextJob()
+
+function executeNextJob(err) {
+	if(jobs.length > nextJob) {
+		jobs[nextJob++](executeNextJob)
+	}
+}
+
+function parseProvisions(done) {
+	provisions.parse(conf.provisions, function(err, parsedProvisions) {
+		conf.parsedProvisions = parsedProvisions
+		done()
+	})
+}
+
+function installProvisions(done) {
+	conf.installedProvisions = provisions.install(conf.parsedProvisions)
+	done()
+}
+
+function addKeychain(done) {
+	exec(
+	  'security'
+	, [ 'default-keychain'
+	  , '-s'
+	  , conf.keychain.path
+	  ]
+	, done
+	)
+}
+
+function unlockKeychain(done) {
+	exec(
+	  'security'
+	, [ 'unlock-keychain'
+	  , '-p'
+	  , conf.keychain.password
+	  , conf.keychain.path
+	  ]
+	, done
+	)
+}
+
+function buildTarget(done) {
+	utils.recurMkdirSync(conf.build.output)
+	exec(
+	  'xcodebuild'
+	, [ '-target'
+	  , conf.build.target
+	  , '-configuration'
+	  , conf.build.configuration
+	  , 'SYMROOT'
+	  , conf.build.output
+	  , 'clean'
+	  , 'build'
+	  ]
+	, done
+	)
+}
+
+function clean(done) {
+	provisions.clean(conf.installedProvisions)
+	done()
+}
